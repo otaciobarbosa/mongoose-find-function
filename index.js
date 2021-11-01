2edb0f428648348b1457014feb044d9bfd22d7dd
Otácio Barbosa <otacio.barbosa@outlook.com>
2021-11-01 17:03:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 const mongoose = require('mongoose');
-  
-// Database connection
+
 mongoose.connect('mongodb://127.0.0.1:27017/geeksforgeeks', {
     useNewUrlParser: true,
+    useCreateIndex: true,
     useUnifiedTopology: true
 });
   
-// User model
 const User = mongoose.model('User', {
     name: { type: String },
     age: { type: Number }
 });
-  
-// Only one parameter [query/condition]
-// Find all documents that matches the
-// condition name='Punit'
+
 User.find({ name: 'Punit'}, function (err, docs) {
     if (err){
         console.log(err);
@@ -24,8 +20,6 @@ User.find({ name: 'Punit'}, function (err, docs) {
     }
 });
   
-// Only Two parameters [condition, query projection]
-// Here age:0 means don't include age field in result 
 User.find({ name: 'Punit'}, {age:0}, function (err, docs) {
     if (err){
         console.log(err);
@@ -35,12 +29,6 @@ User.find({ name: 'Punit'}, {age:0}, function (err, docs) {
     }
 });
   
-// All three parameter [condition, query projection,
-// general query options]
-// Fetch first two records whose age >= 10 
-// Second parameter is null i.e. no projections
-// Third parameter is limit:2 i.e. fetch
-// only first 2 records
 User.find({ age: {$gte:10}}, null, {limit:2}, function (err, docs) {
     if (err){
         console.log(err);
